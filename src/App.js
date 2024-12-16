81ff23cd0c704c12b581afddeb9ff76093d62135
Levansarajishvili <120027976+Levansarajishvili@users.noreply.github.com>
2024-12-16 09:56:09 +0400

    scrollsection

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ScrollToSection from './components/scroll-to-section'
+
+function App() {
+  return (
+    <div className="App">
+      <ScrollToSection />
+    </div>
+  );
+}
+
+export default App;
